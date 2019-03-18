7f0457522e931e307297916f50c19213fbeeecf4
jugel <jugel@gmail.com>
2019-03-18 21:46:42 +0800

    7

--- swni/src/main/Berm.java
+++ swni/src/main/Berm.java
@@ -5,6 +5,6 @@ public class Berm {
 		System.out.println("maybe you are right");
 		System.out.println("maybe you are right");
 		System.out.println("maybe you are right");
-		System.out.println("maybe you are right5");
+		System.out.println("maybe you are right5...");
 	}
 }
